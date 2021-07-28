0270cd25936cab68d77ee96ca947b727c4594254
Oluwasegun <ilesanmioluwasegun1989@gmail.com>
2021-07-27 20:12:38 -0500

    Icons, Favorites, and Comments

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { View } from 'react-native';
 import Main from './components/MainComponent';
 
 
 export default function App() {
   return (
-    <Main />
+      <Main />
   );
 }
 
